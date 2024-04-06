101d8fd00fb4b8020e7e8a485ab5c3dbee5d4682
Hillary-Ndubi <hillarynyabaige@gmail.com>
2024-04-06 18:34:37 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+
+    let config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", config)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error.message));
+
+}
